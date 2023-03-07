659221e70abf577fd1d51c52d4d99526e90f4424
Guilherme Rodrigues Jordão dos Santos <90724400+GuilhermeJordao@users.noreply.github.com>
2023-03-07 20:53:38 -0300

    Correção e Adição de Rotas
    Adição e Correção de Rotas para as telas existentes Login e Cadastro além de funcionalidade de Redirecionamento Botão Superior no canto esquerdo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Telas/login/login.component';
+import { CadastroUsuComponent } from './Telas/cadastro-usu/cadastro-usu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:LoginComponent},
+  {path: 'cadastro', component:CadastroUsuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
