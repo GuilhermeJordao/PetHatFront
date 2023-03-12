607e42bf6d176b9184aaed561563af876514587f
KawanXLima <kawan.ximendes@gmail.com>
2023-03-12 17:44:55 -0300

    mensagem de erro e mensagem de sucesso
    Foi criado duas divs uma para erro e outra para sucesso. A de sucesso tem redirecionamento para a tela de login, ja a de erro tem somente sua aparição durante determinado tempo.

--- src/app/Telas/cadastro-usu/cadastro-usu.component.ts
+++ src/app/Telas/cadastro-usu/cadastro-usu.component.ts
@@ -1,6 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
 import { ClienteService } from '../service/cliente.service';
 @Component({
   selector: 'app-cadastro-usu',
@@ -9,11 +9,13 @@ import { ClienteService } from '../service/cliente.service';
 })
 export class CadastroUsuComponent implements OnInit {
   form: FormGroup;
+  erroMensagem = false;
+  sucessoMensagem = false;
 
   constructor(
     private clienteService: ClienteService,
     private formBuilder: FormBuilder,
-    private toastr:ToastrService
+    private localPagina: Location
   ) {
     this.form = this.formBuilder.group({
       nome: [null],
@@ -24,13 +26,22 @@ export class CadastroUsuComponent implements OnInit {
   }
 
   onSubmit() {
-    this.clienteService.save(this.form.value).subscribe((dados) => {
-      console.log(dados);
-    },(error)=> this.Error());
-  }
-
-  private Error() {
-    this.toastr.error('Ops','Parece que algo deu errado :(');
+    this.clienteService.save(this.form.value).subscribe({
+      next: (dados) => {
+        this.sucessoMensagem = true;
+        setTimeout(() => {
+          this.sucessoMensagem = false;
+          this.localPagina.back();
+        }, 3000);
+      },
+      error: (e) => {
+        this.erroMensagem = true;
+        setTimeout(() => {
+          this.erroMensagem = false;
+        }, 5000);
+      },
+      complete: () => console.log('completo'),
+    });
   }
 
   ngOnInit() {}
